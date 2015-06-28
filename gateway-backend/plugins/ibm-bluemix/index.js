d54e130f58392efb0282dfca060e5352816cb468
IoTboy <naga.kona@outlook.com>
2015-06-27 17:16:57 -0700

    IBM Bluemix plugin reorg

--- gateway-backend/plugins/ibm-bluemix/index.js
+++ gateway-backend/plugins/ibm-bluemix/index.js
@@ -1,130 +1,75 @@
-var mqtt = require('mqtt');
-var getmac = require('getmac');
-var https = require('https');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var sleep = require('sleep');
 
-// IBM BluxMix MQTT port
-var port = 1883;
+function ibm(json){
+  var self = this;
+  self.dbName = json.db;
+  self.url = json.url;
+  self.dbClient;
 
-module.exports = {
+  ibm.prototype.connect = function() {
+    console.log('Entered connect function! DB : ' + this.dbName);
+    this.db = MongoClient.connect(self.url, {
+    db: {
+      native_parser: false
+    },
+    server: {
+      socketOptions: {
+        connectTimeoutMS: 5000
+      }
+    },
+    replSet: {},
+    mongos: {}
+  }, function(err, db) {
+      if(err) {
+        console.log("failed to connect to the database");
+      } else {
+	console.log("Connect - connected to database");
+	self.dbClient = db;
+      }
+    } );
+  };
 
-    connect: function(json) {
-
-        console.log("Entered the connection function");
-        this.config = json;
-
-	getmac.getMac(function(err, macAddress) {
-    	    if (err) throw err;
-
-    	    macAddress = macAddress.toString().replace(/:/g, '').toLowerCase();
+  ibm.prototype.write = function(data) {
+    console.log('Entered write function!');
+    this.db = MongoClient.connect(self.url, function(err, db) {
+      if(err) {
+        console.log("failed to connect to the database");
+      } else {
+	console.log("Write - connected to database");
+        this.collection = self.dbClient.collection(self.dbName);
+        this.collection.insert(data, function(err, result) {
+          if(err) {
+	    console.log("Data sending failed");
+          } else {
+	    console.log("Data sent successfully")
+          }
         });
+      }
+    });
+  };
 
-	if(this.config) {
-
-	    if(this.config['auth-method'] && (this.config['auth-method'] !== "token")){
-	        throw "Authentication method not supported. Please make sure to use \"token\".";
+  ibm.prototype.read = function(query) {
+    console.log("Entered read :" + query.timestamp);
+    this.db = MongoClient.connect(self.url, function(err, db) {
+      if(err) {
+      	console.log("Read - failed to connect to the database");
+      } else {
+	console.log("Read - connected to database");
+      	this.collection = db.collection(self.dbName);
+        this.collection.find(
+	  { "timestamp": { $gt: query.timestamp } } ).toArray(function(err, items) {
+	    if(err) {
+	      console.log("Data Receive Failed");
+	    } else {
+	       console.log("Data received");
+	       console.log(items);
 	    }
+	  });
+      }
+    });
+  };
+}
 
-	    if(!this.config['auth-token']) {
-	        throw "Auth token not found";
-	    }
-
-	    if(!this.config.org){
-                throw "Configuration should include an org field that specifies your organization.";
-            }
-
-            if(!this.config.type){
-                throw "Configuration should include a type field that specifies your device type.";
-            }
-
-            if(!this.config.id){
-	        this.config.id = macAddress;
-                throw "Configuration should include an id field that specifies your device id.";
-            }
-
-	    this.organization = this.config.org;
-            this.deviceType = this.config.type;
-            this.macAddress = this.config.id;
-	    this.port = this.config.port;
-
-            this.broker = this.organization + ".messaging.internetofthings.ibmcloud.com";
-	}
-	else {
-	    console.log("No configuration file found.");
-	}
-
-	client = mqtt.connect(
-            {
-                host: this.broker,
-                port: this.port,
-                username: this.config['username'],
-                password : this.config['auth-token'],
-                clientId : "d:" + this.organization + ":" + this.deviceType + ":" + this.macAddress
-            });
-        //      console.log(client);
-
-    },
-
-    // Store the data record with the evt_type equal to the devID
-    // This allows us to retrieve the records based on devID
-
-    write: function(data) {
-	topic = "iot-2/evt/" + this.devId + "/fmt/json";
-	var message = {
-            "d" : {
-                "value" : data.value,
-                "sensorType" : data.sensorType,
-                "timestamp" : data.timestamp
-            }
-        };
-
-        //	console.log(message);
-        client.publish(topic, JSON.stringify(message));
-    },
-
-
-    read: function() {
-
-        console.log("here 1");
-        var options = {
-            hostname: "internetofthings.ibmcloud.com"
-            ,port: 443
-            ,auth: {
-                user: "a-5anfde-xxkarh3mxp"
-                ,pass: "pkaXR)SEnVrsuM21BY"
-            }
-            ,path: '/api/v0001/historian/5anfde/intel-edison?evt_type=temperatureEvent'
-            ,method: 'GET'
-            ,headers: { 'Content-Type': 'application/json' }
-        };
-        console.log("here 2");
-
-        var req = https.get(options, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-            // Buffer the body entirely for processing as a whole.
-            var bodyChunks = [];
-            res.on('data', function(chunk) {
-                // You can process streamed parts here...
-                bodyChunks.push(chunk);
-            }).on('end', function() {
-                var body = Buffer.concat(bodyChunks);
-                console.log('BODY: ' + body);
-                // ...and/or process the entire body here.
-            });
-        });
-
-        req.on('error', function(e) {
-            console.log('ERROR: ' + e.message);
-        });
-
-        console.log("here 6");
-        req.end();
-        console.log("here 7");
-    },
-
-
-    disconnect: function() {
-
-    }
-};
+module.exports = ibm;
--- gateway-backend/plugins/ibm-bluemix/test/test1.js
+++ gateway-backend/plugins/ibm-bluemix/test/test1.js
@@ -1,16 +1,41 @@
 var ibm = require("../index.js");
 var config = require("../config.json");
+var sleep = require('sleep');
 
-ibm.connect(config);
-ibm.read();
+var ibm = new ibm(config);
+ibm.connect();
 
-ibm.write({
-    devId : "5678",
+count = 0;
+var result = [];
+
+
+while (count < 5)
+{
+  sleep.sleep(1);
+  result.push({ devId : 'sensor-4321',
+		value : Math.floor((Math.random() * 30) + 60),
+		sensorType : "temp",
+		timestamp : Math.floor(Date.now()/1000)
+	      });
+  count++;
+}
+
+//console.log(result);
+
+data = {
+    devId: "5678",
     value : 80,
     sensorType : "temp",
     timestamp : Date.now()
+}
+
+ibm.write(result);
+
+var date = new Date();
+date.setDate(date.getDate() - 1);
+
+
+ibm.read({
+     "devId" : "sensor-4321",
+     "timestamp" : Date.parse(date)/1000
 });
-
-
-
-console.log("Successful write ... I think");
