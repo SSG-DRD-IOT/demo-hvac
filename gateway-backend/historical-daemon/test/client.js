51185d5af205e2aa4b82efca31952f6af056fe09
IoTboy <naga.kona@outlook.com>
2015-06-27 17:39:02 -0700

    Client for testing historic-daemon

---
+++ gateway-backend/historical-daemon/test/client.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var options = {
+            hostname: "localhost"
+            ,port: 4000
+            ,path: '/api/v0001/historic/data'
+            ,method: 'GET'
+        };
+
+var req = http.get(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+            // Buffer the body entirely for processing as a whole.
+        res.on('data', function(chunk) {
+           console.log('BODY: ' + chunk);
+        });
+});
+
+        req.on('error', function(e) {
+            console.log('ERROR: ' + e.message);
+        });
