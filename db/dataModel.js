b71ebde9e96e55a99971969f34938a08ba54dd8c
Daniel Holmlund <agnathan@gmail.com>
2015-06-28 08:38:50 -0700

    Applied callback best practices
    added return statements to callback invocations
    removed unnecessary DB transactions
    removed unnecessary console.log statements

--- db/dataModel.js
+++ db/dataModel.js
@@ -8,37 +8,17 @@ var DataModel = function(db, data) {
 
     // INSERT INTO "main"."data" ("sensor_id","value") VALUES (?1,?2)
     this.save = function() {
-        console.log(this.data);
-        console.log("Just before DB transaction");
-        this.db.run("BEGIN TRANSACTION");
         this.db.run('INSERT INTO "main"."data" ("sensor_id","value", "timestamp") VALUES (?1,?2,?3)', [this.data.sensor_id, this.data.value, this.data.timestamp]  );
-        this.db.run("END");
     };
-
-    //this.find_by_id = function(condition) {
-    //     this.db.each("SELECT * FROM sensors WHERE id = ?", condition, function(err, row) {
-    //         console.log(row);
-    //         console.log("--------------");
-    //     });
-    // };
-
-
-    //this.find_by_id = function(condition) {
-    //     this.db.each("SELECT * FROM sensors WHERE id = ?", condition, function(err, row) {
-    //         console.log(row);
-    //         console.log("--------------");
-    //     });
-    // };
 
     this.find = function(callback) {
         var sql = "SELECT * FROM data";
 
         this.db.all(sql,  function(err, results) {
             if (err) {
-                callback(err, results);
+                return callback(err);
             } else {
-                //                delete_by_sensor_id(
-                callback(null, results);
+                return callback(err, results);
             }
         });
     };
@@ -49,18 +29,15 @@ var DataModel = function(db, data) {
 
         this.db.all(sql,  function(err, results) {
             if (err) {
-                callback(err, results);
+                return callback(err, results);
             } else {
-//                delete_by_sensor_id(
-                callback(null, results);
+                return callback(null, results);
             }
         });
     };
 
     this.delete_by_sensor_id = function(id) {
-        this.db.run("BEGIN TRANSACTION");
         this.db.run("DELETE from sensors WHERE id = ?", id);
-        this.db.run("END");
     };
 
     this.delete_all_data = function(id) {
