8fc3bf5c598a7fe0d8aa4fa740eb73645eef7483
Daniel Holmlund <agnathan@gmail.com>
2015-07-27 10:12:23 -0700

    Setup Continuous Integration with TestemJS and Grunt
    Set the default grunt task to run the continuous integration testing.
    
    Included several dev plugins in the db directory
    npm install grunt-contrib-testem --save-dev
    npm install sinon --save-dev
    npm install grunt-contrib-testem --save-dev

---
+++ db/Gruntfile.js
@@ -0,0 +1,48 @@
+var grunt = require('grunt');
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+            all: ['src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    _: false,
+                    $: false,
+                    jasmine: false,
+                    describe: false,
+                    it: false,
+                    expect: false,
+                    beforeEach: false,
+                    afterEach: false,
+                    sinon: false
+                },
+                browser: true,
+                devel: true
+            }
+        },
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmine2',
+                    launch_in_dev: ['PhantomJS'],
+                    before_tests: 'grunt jshint',
+                    serve_files: [
+                        'node_modules/lodash/index.js',
+                        'node_modules/jquery/dist/jquery.js',
+                        'node_modules/sinon/pkg/sinon.js',
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ],
+                    watch_files: [
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ]
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-testem');
+
+    grunt.registerTask('default', ['testem:run:unit']);
+};
