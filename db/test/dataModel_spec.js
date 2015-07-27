0d5b6dca6af64c0e2d6c2a9324f6902de8dc2272
Daniel Holmlund <agnathan@gmail.com>
2015-07-27 10:42:03 -0700

    Created a Fixtures Model for Sample test data
    The test data can now be imported from a separate fixtures file.
    DataModels are named as follows:
    valid_data_1, valid_data_2, invalid_sensor_reading, invalid_timestamp,
    invalid_sensor_id

--- db/test/dataModel_spec.js
+++ db/test/dataModel_spec.js
@@ -1,14 +1,7 @@
 var dataModel = require('../src/dataModel');
-describe("dataModel", function(){
+var fixtures = require('fixtures/data');
 
-    var fixtures = [
-        {"sensor_id": "1", "value": "74.54047151375562", "timestamp":"1438015679772"},
-        {"sensor_id": "2", "value": "65.22398894652724", "timestamp":"1438015679772"},
-        {"sensor_id": "1", "value": "71.36048490181565", "timestamp":"1438015681778"},
-        {"sensor_id": "2", "value": "65.37656239140779", "timestamp":"1438015681778"},
-        {"sensor_id": "1", "value": "83.30243791919202", "timestamp":"1438015683780"},
-        {"sensor_id": "2", "value": "64.37094074301422", "timestamp":"1438015683780"}
-    ];
+describe("dataModel", function(){
 
     var currentDataModel = null;
     beforeEach(function(done){
