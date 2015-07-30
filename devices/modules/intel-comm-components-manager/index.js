8dbcb2ff704736a5639f0f61ab853099a07f4e92
Drew Williams <willdrewm@gmail.com>
2015-07-30 17:49:31 +0000

    Modified the intel-comm-components-manager to use UPM, switch statements across sensor config files (instead of comparing name strings).

--- devices/modules/intel-comm-components-manager/index.js
+++ devices/modules/intel-comm-components-manager/index.js
@@ -12,24 +12,31 @@ var relay = require('intel-comm-components-relay')
 //for an actuator.  
 exports.getComponent = function getComponent(name, pin)
 {
+    
   var returnedComponent = {};
   
-  //Note: the below is bad practice, and will be changed eventually.
-  if (name === "light")
-  {
-    returnedComponent = new groveSensor.GroveLight(+pin);
-  }
-  if (name === "sound")
-  {
-    returnedComponent = new loudnessSensor.GroveLoudness(+pin);
-  }
-  if (name === "temperature")
-  {
-    returnedComponent = new groveSensor.GroveTemp(+pin);
-  }
-  if (name === "relay")
-  {
-    returnedComponent = relay;
+  //As of 7-30, the configurations for each sensor have a general ID.  The ID is just a number.  
+  //ID corresponds to a particular sensor, so it is NOT unique, but differentiates between temperature
+  //and sound sensors, for example.
+  var configs = require('./'+name+'.json');
+  var generalID = configs.genid;
+  
+  //Now we perform a switch statement on the generalID that we used.
+  switch (+generalID) {
+      case 0:
+           returnedComponent = new upm.GroveTemp(+pin);
+          break;
+      case 1:
+          returnedComponent = new upm.GroveLight(+pin);
+          break;
+      case 2:
+          returnedComponent = new upm.GroveLoudness(+pin);
+          break;
+      case 3:
+          returnedComponent = relay;
+          break;
+      default:
+          returnedComponent = NULL;
   }
   
   //Note that it's not required that returnedComponent is assigned to anything, so it could be NULL in some
