d5992bb6c3cff8e52fceee45e182896234788453
IoTboy <naga.kona@outlook.com>
2015-08-03 19:51:54 -0700

    Initial file renames for plugins

---
+++ gateway-backend/plugins/ibm-bluemix/index.js
@@ -0,0 +1,130 @@
+var mqtt = require('mqtt');
+var getmac = require('getmac');
+var https = require('https');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+// IBM BluxMix MQTT port
+var port = 1883;
+
+var bluemix = function(json){
+
+  var self = this;
+
+  this.on('connect', function() {
+    // console.log("Entered the connection function");
+    this.config = json;
+
+    getmac.getMac(function(err, macAddress) {
+      if (err) throw err;
+      macAddress = macAddress.toString().replace(/:/g, '').toLowerCase();
+    });
+
+    if(this.config) {
+
+      if(this.config['auth-method'] && (this.config['auth-method'] !== "token")){
+        throw "Authentication method not supported. Please make sure to use \"token\".";
+      }
+
+      if(!this.config['auth-token']) {
+        throw "Auth token not found";
+      }
+
+      if(!this.config.org){
+        throw "Configuration should include an org field that specifies your organization.";
+      }
+
+      if(!this.config.type){
+        throw "Configuration should include a type field that specifies your device type.";
+      }
+
+      if(!this.config.id){
+        this.config.id = macAddress;
+        throw "Configuration should include an id field that specifies your device id.";
+      }
+
+      this.organization = this.config.org;
+      this.deviceType = this.config.type;
+      this.macAddress = this.config.id;
+      this.port = this.config.port;
+
+      this.broker = this.organization + ".messaging.internetofthings.bluemixcloud.com";
+    }
+    else {
+      console.log("No configuration file found.");
+    }
+
+    client = mqtt.connect(
+      {
+        host: this.broker,
+        port: this.port,
+        username: this.config['username'],
+        password : this.config['auth-token'],
+        clientId : "d:" + this.organization + ":" + this.deviceType + ":" + this.macAddress
+      });
+      //      console.log(client);
+
+    });
+
+    // Store the data record with the evt_type equal to the devID
+    // This allows us to retrieve the records based on devID
+
+    this.on('write', function() {
+      topic = "iot-2/evt/" + this.devId + "/fmt/json";
+      var message = {
+        "d" : {
+          "value" : data.value,
+          "sensorType" : data.sensorType,
+          "timestamp" : data.timestamp
+        }
+      };
+
+      //	console.log(message);
+      client.publish(topic, JSON.stringify(message));
+    });
+
+
+    this.on('read', function() {
+
+      console.log("Bluemix - In read");
+      var start = Date.now() - 10;
+
+      var options = {
+        hostname: "internetofthings.bluemixcloud.com"
+        ,port: 443
+        ,path: '/api/v0001/historian/ndag4d?start=78787676&end=' + Date.now()
+        ,method: 'GET'
+        ,headers: {
+          'Authorization': 'Basic ' + new Buffer("a-ndag4d-3p2uynctid" + ":" + "0+ab38e2gGOjiI9m(V").toString('base64')
+        }
+      };
+      console.log("here 2");
+
+      var req = https.get(options, function(res) {
+        // console.log('STATUS: ' + res.statusCode);
+        // console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+        // Buffer the body entirely for processing as a whole.
+        var body = "";
+        res.on('data', function(data) {
+          // You can process streamed parts here...
+          body += data;
+        }).on('end', function() {
+          console.log('BODY: ' + body);
+          // ...and/or process the entire body here.
+        });
+      });
+
+      req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+      });
+
+      console.log("here 6");
+      req.end();
+      console.log("here 7");
+    });
+
+  }
+
+  util.inherits(bluemix, EventEmitter);
+  module.exports = bluemix;
