ea9d269345e6e5cb08e257ff25ae4ebc4afff86d
vaghesh <vaghesh@gmail.com>
2015-08-05 17:17:41 -0700

    Fixed Button in Deleting trigger and Adding trigger
    Problem was Controller was not loading for the page

--- gateway-frontend/app/scripts/controllers/table.js
+++ gateway-frontend/app/scripts/controllers/table.js
@@ -13,28 +13,7 @@
 
  .controller('TableCtrl',['$scope','$location','$http', '$window',function($scope,$location,$http,$window) {
 
-  // To get data from actuator table
-  $http.get('/listActuator').success(function(data,status) {
-    $scope.actuators = data;
-  });
-
-  // To get data from Sensor table
-   $http.get('/listSensor').success(function(data,status) {
-    $scope.sensors = data;
-  });
-
-   // To get data from Trigger table
-   $http.get('/listTrigger').success(function(data,status) {
-    $scope.triggers = data;
-  });
-
-   $scope.url = $scope.swaggerUrl = 'http://10.246.15.211:10010';
   
-   $scope.IsVisible = false;
-   $scope.ShowHide = function () {
-		//If DIV is visible it will be hidden and vice versa.
-		$scope.IsVisible = $scope.IsVisible ? false : true;
-   }
 
 
    
