23f58e921003b8660f0855880f905bea6267af1f
Daniel Holmlund <agnathan@gmail.com>
2015-08-07 20:49:34 -0700

    Adding Swagger scaffold

--- devices/modules/intel-comm-transport-mqtt/index.js
+++ devices/modules/intel-comm-transport-mqtt/index.js
@@ -5,53 +5,53 @@ var mqtt = require('mqtt');
 exports.connectClient = function initialConnect(ip)
 //Connects to an MQTT server @ 'ip'.  Returns the connected client.
 {
-   var client = mqtt.connect(ip);
-   client.on('connect', function ()
-                       {
-                           console.log("Connected to server.");
-                       });
-   return client;
-}
+    var client = mqtt.connect(ip);
+    client.on('connect', function ()
+              {
+                  console.log("Connected to server.");
+              });
+    return client;
+};
 
 //Publishes an announcement string on the string channel
 exports.publishAnnouncement = function publishAnnouncement(announceJSON, client)
 {
     var channelTitle = "announcements";
     client.publish(channelTitle,announceJSON);
-}
+};
 
 //Subscribes to the control channel on the gateway
 exports.subscribeToControl = function subControl(channelTitle, client)
 {
     //var channelTitle = gatewayDevID+'-control';
     client.subscribe(channelTitle);
-}
+};
 
 exports.unsubToControl = function unsubControl (channelTitle, client)
 {
     //var channelTitle = gatewayDevID+'-control';
     client.unsubscribe(channelTitle);
-}
+};
 
 //Publishes data to the data channel.
 exports.publishData = function publishData(devID, client, string)
 {
-  //Print to the server
-  var channelTitle = "sensors/"+devID+"/data";
-  client.publish(channelTitle, string);
-  console.log("Published "+string+ " to "+devID);
-}
+    //Print to the server
+    var channelTitle = "sensors/"+devID+"/data";
+    client.publish(channelTitle, string);
+    console.log("Published "+string+ " to "+devID);
+};
 
 //Publishes errors to the error channel.
 exports.publishErrors = function publishData(devID, client, string)
 {
-  //Print to the server
-  var topicTitle = "other/"+deviceID+"/errors"
-  client.publish('data', string);
-}
+    //Print to the server
+    var topicTitle = "other/"+deviceID+"/errors";
+    client.publish('data', string);
+};
 
 //Closes the connection
 exports.closeConnect = function closeConnection(client)
 {
-  client.end();
-}
+    client.end();
+};
