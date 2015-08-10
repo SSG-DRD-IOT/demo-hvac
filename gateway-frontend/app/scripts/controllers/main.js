5b1e7f639a8e006df8a3faa4c6a9dcb366b47ce2
vaghesh <vaghesh@gmail.com>
2015-08-09 22:09:23 -0700

    Implemented Rickshaw Graph

--- gateway-frontend/app/scripts/controllers/main.js
+++ gateway-frontend/app/scripts/controllers/main.js
@@ -83,7 +83,18 @@ angular.module('sbAdminApp')
 	
 	
 	
-	
+	 $scope.options1 = {
+                    renderer: 'area'
+                };
+                $scope.series1 = [{
+                        name: 'Series 1',
+                        color: 'steelblue',
+                        data: [{x: 0, y: 23}, {x: 1, y: 15}, {x: 2, y: 79}, {x: 3, y: 31}, {x: 4, y: 60}]
+                    }, {
+                        name: 'Series 2',
+                        color: 'lightblue',
+                        data: [{x: 0, y: 30}, {x: 1, y: 20}, {x: 2, y: 64}, {x: 3, y: 50}, {x: 4, y: 15}]
+                    }];
 	
 	
 	
