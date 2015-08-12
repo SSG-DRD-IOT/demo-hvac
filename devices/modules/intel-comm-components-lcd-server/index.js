c13a6c2181b4f49a0df043b7992469c41d18ca83
Drew Williams <willdrewm@gmail.com>
2015-08-12 11:41:26 -0700

    Continued fixes to LCD server

--- devices/modules/intel-comm-components-lcd-server/index.js
+++ devices/modules/intel-comm-components-lcd-server/index.js
@@ -10,7 +10,7 @@ var errorChannel = "sensors/temperature/errors";
 var alertChannel = "sensors/temperature/alerts";
 var lcdMessage = ""; //Blank space for the LCD message.
 var myLCD = new LCD.Jhd1313m1(6, 0x3E, 0x62); //"address" for the RGB LCD
-
+var parsedMessage = {};
 var client  = mqtt.connect('mqtt://192.168.1.1');
 
  //When we connect, we're targeting the temperature sensor for the demo -
@@ -20,66 +20,65 @@ client.on('connect', function ()
   client.subscribe(dataChannel);
   client.subscribe(errorChannel);
   client.subscribe(alertChannel);
-  myLCD.setColor(0,0,0);
+  myLCD.setColor(255,255,255);
 });
 
 client.on('message', function (topic, message)
 {
+  try {
+    parsedMessage = JSON.parse(message);
+    console.log("Json Received: "+message);
+  } catch (error) {
+    throw(error);
+  }
   // message is Buffer
   if (topic == dataChannel)
   {
+    console.log("Data received!");
     //Print the new temperature to the LCD screen.
     myLCD.write("");
-    lcdMessage = JSON.parse(message).value + " celsius";
+    lcdMessage = parsedMessage.value + " celsius   ";
     myLCD.setCursor(0,1);
     myLCD.write(lcdMessage);
   }
   if (topic == alertChannel)
   {
-
+    console.log("Alert was received!");
     //If it's too hot...
-    if (JSON.parse(message).alert === "Hot") //If the temperature sensor is too hot...
+    if (parsedMessage.alert === "Hot") //If the temperature sensor is too hot...
     {
       //Set the screen color to red.
       myLCD.setColor(255,0,0);
     }
     //If it's too cold...
-    if (JSON.parse(message).alert === "Cold" ) //If the temperature sensor is too cold...
+    if (parsedMessage.alert === "Cold" ) //If the temperature sensor is too cold...
     {
       //Set the screen color to blue
       myLCD.setColor(0,0,255);
    }
-   //If it's too cold...
-   if (JSON.parse(message).alert === "Ok" ) //If the temperature sensor is too cold...
+   //If it's OK...
+   if (parsedMessage.alert === "Ok" ) //If the temperature sensor is too cold...
    {
-     //Set the screen color to blue
-     myLCD.setColor(0,0,0);
+     //Set the screen color to neutral
+     myLCD.setColor(255,255,255);
    }
   }
   else if (topic == errorChannel)
   {
-    if (message === "HotError" )   //If there's another error condition...
+    console.log("Oh no!  Error received!");
+    if (parsedMessage.alert === "HotError" )   //If there's another error condition...
     {
-      for (i = 0; i < 10; i++)
-      {
-        myLCD.setColor(0,0,0);
-        setTimeout(function()
-        {
-          myLCD.setColor(255,0,0);
-        }, 1000);
-        delay(1000);
-      }
+      myLCD.setColor(255,255,0);
+      setTimeout( function() {
+        myLCD.setColor(255,255,255);
+      }, 1000);
     }
-    if (message === "ColdError" )   //If there's another error condition...
+    if (parsedMessage.alert === "ColdError" )   //If there's another error condition...
     {
-      for (i = 0; i < 10; i++)
-      {
-        myLCD.setColor(0,0,0);
-        setTimeout(function()
-        {
-          myLCD.setColor(255,0,0);
-        }, 1000);
-        delay(1000);
-      }
+      myLCD.setColor(255,0,255);
+      setTimeout( function() {
+        myLCD.setColor(255,255,255);
+      }, 1000);
     }
+  }
 });
