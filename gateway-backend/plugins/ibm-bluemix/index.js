3c535091578143ad2358812855e1d49c83d25c0a
IoTboy <naga.kona@outlook.com>
2015-08-12 19:19:39 -0700

    some more

---
+++ gateway-backend/plugins/ibm-bluemix/index.js
@@ -0,0 +1,87 @@
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+// Lodash is a functional library for manipulating data structures
+var _ = require("lodash");
+
+// Setup a logging system in this plugin
+var logger = require('./logger.js');
+
+function ibm(json){
+  var self = this;
+  self.config = json;
+  self.db;
+  self.dataQuery = {};
+
+  if(self.config.debug != "true") {
+    logger.remove(winston.transports.Console);
+    logger.remove(winston.transports.File);
+  }
+
+
+  ibm.prototype.connect = function() {
+    // logger.info("Entered connect function! DB : " + self.config.db);
+    self.db = MongoClient.connect(self.config.url, function(err, db) {
+      if(err) {
+        logger.error("IBM - Connection failed");
+        logger.error(err);
+      } else {
+        logger.info("IBM - Connected successfully");
+      }
+    } );
+  };
+
+  ibm.prototype.write = function(data) {
+    // logger.log('Entered write function!');
+    self.db = MongoClient.connect(self.config.url, function(err, db) {
+      if(err) {
+        logger.error("IBM - Connection failed");
+        logger.error(err);
+      } else {
+        logger.info("IBM - Connected successfully");
+        self.collection = db.collection(self.config.db);
+        self.collection.insert(data, function(err, result) {
+          if(err) {
+            logger.error("IBM - Data sending failed");
+            logger.error(err);
+          } else {
+            logger.info("IBM - Data sent successfully");
+            // logger.info(result);
+          }
+        });
+      }
+    });
+  };
+
+  ibm.prototype.read = function(readQuery, callback) {
+    // logger.log("Entered read :" + query.timestamp);
+    if(readQuery.timestamp) {
+      self.dataQuery = { "timestamp": { $gt: readQuery.timestamp } };
+    } else if(readQuery.sensor_id) {
+      self.dataQuery = { "sensor_id": { $eq: readQuery.sensor_id } };
+    }
+    self.db = MongoClient.connect(self.config.url, function(err, db) {
+      if(err) {
+        logger.error("IBM - Connection failed");
+        logger.error(err);
+      } else {
+        logger.info("IBM - Connected successfully");
+        self.collection = db.collection(self.config.db);
+        self.collection.find(self.dataQuery).toArray(
+          function(err, items) {
+            if(err) {
+              logger.error("IBM - Data receive failed");
+              logger.error(err);
+            } else {
+              _.map(items, function(d) {delete d._id;});
+              logger.info("IBM - Data received: %d", items.length);
+              // logger.info(JSON.stringify(items, null, '  '));
+            }
+            callback(err, items);
+          });
+        }
+      });
+    };
+  }
+
+  module.exports = ibm;
--- gateway-backend/plugins/microsoft-azure/index.js
+++ gateway-backend/plugins/microsoft-azure/index.js
@@ -12,109 +12,109 @@ function azure(json) {
   self.client;
   self.dataQuery = {};
 
-    if(self.config.debug != "true") {
-      logger.remove(winston.transports.Console);
-      logger.remove(winston.transports.File);
+  if(self.config.debug != "true") {
+    logger.remove(winston.transports.Console);
+    logger.remove(winston.transports.File);
+  }
+
+  azure.prototype.connect = function(callback){
+
+    //  logger.info('Azure: Entered connect function');
+    azureTable.setDefaultClient({
+      accountUrl: 'http://' + self.config.accountName + '.table.core.windows.net/',
+      accountName: self.config.accountName,
+      accountKey: self.config.accessKey,
+      timeout: 10000
+    });
+
+    self.client = azureTable.getDefaultClient();
+
+
+    if(self.client) {
+      self.client.createTable(self.config.table, function(err, resp) {
+        if(err) {
+          if(err.statusCode == 409) {
+            result = true;
+            logger.info('Azure - Table already exists');
+          } else {
+            logger.error('Azure - Table creation failed');
+            logger.error('Azure - Connection failed');
+            logger.error(err);
+            result = false;
+          }
+        } else {
+          result = true;
+          logger.info('Azure - Connected successfully');
+        }
+        callback(result);
+      });
+    } else {
+      logger.error('Azure - Connection failed');
     }
 
-    azure.prototype.connect = function(callback){
-
-      //  logger.info('Azure: Entered connect function');
-      azureTable.setDefaultClient({
-        accountUrl: 'http://' + self.config.accountName + '.table.core.windows.net/',
-        accountName: self.config.accountName,
-        accountKey: self.config.accessKey,
-        timeout: 10000
-      });
-
-      self.client = azureTable.getDefaultClient();
+  };
 
 
-      if(self.client) {
-        self.client.createTable(self.config.table, function(err, resp) {
-          if(err) {
-            if(err.statusCode == 409) {
-              result = true;
-              logger.info('Azure - Table already exists');
-            } else {
-              logger.error('Azure - Table creation failed');
-              logger.error('Azure - Connection failed');
-              logger.error(err);
-              result = false;
-            }
-          } else {
-            result = true;
-            logger.info('Azure - Connected successfully');
-          }
-          callback(result);
-        });
-      } else {
-        logger.error('Azure - Connection failed');
-      }
+  azure.prototype.write = function(data) {
 
-    };
+    // logger.info('Azure - Write function');
+
+    // Batch write does n't seem to work
+    //  var batchClient = self.client.startBatch();
 
 
-    azure.prototype.write = function(data) {
+    for(i in data) {
+      entity = data[i];
+      entity['PartitionKey'] = entity.sensor_id.toString();
+      entity['RowKey'] = entity.timestamp.toString();
 
-      // logger.info('Azure - Write function');
+      // Logging entities to be sent
+      // logger.info(data);
 
-      // Batch write does n't seem to work
-      //  var batchClient = self.client.startBatch();
-
-
-      for(i in data) {
-        entity = data[i];
-        entity['PartitionKey'] = entity.sensor_id.toString();
-        entity['RowKey'] = entity.timestamp.toString();
-
-        // Logging entities to be sent
-        // logger.info(data);
-
-        self.client.insertOrReplaceEntity(self.config.table, entity, function(err, results){
-          if(err){
-            logger.error('Azure - Data sending failed');
-            logger.error(err);
-          } else {
-            logger.info('Azure - Data sent successfully');
-            //logger.info(results);
-          }
-        });
-      }
-
-    };
-
-
-    azure.prototype.read = function(readQuery, callback) {
-
-      //  logger.log('Azure - Read function');
-      if(readQuery.timestamp) {
-        self.dataQuery = azureTable.Query.create('PartitionKey', '==', readQuery.sensor_id).and('timestamp', '>=', readQuery.timestamp);
-      } else {
-        self.dataQuery = azureTable.Query.create('PartitionKey', '==', readQuery.sensor_id);
-      }
-
-      self.client.queryEntities(self.config.table, {
-        query: self.dataQuery
-        // onlyFields: ['sensor_id', 'timestamp', 'value']
-      }, function(err, results, continuation) {
-        if(err) {
-          logger.error('Azure - Data read failed');
+      self.client.insertOrReplaceEntity(self.config.table, entity, function(err, results){
+        if(err){
+          logger.error('Azure - Data sending failed');
+          logger.error(err);
         } else {
-          _.map(results, function(d) {delete d.Timestamp; delete d.RowKey; delete d.PartitionKey});
-          logger.info("Azure - Data recieved: %d", results.length);
+          logger.info('Azure - Data sent successfully');
           //logger.info(results);
         }
-        callback(err, results);
       });
-    };
+    }
 
-    azure.prototype.delete = function() {
-      self.client.deleteTable(self.config.table, function (err, resp) {
-        if(err) logger.error(err);
-        else logger.info('Table deleted');
-      });
-    };
+  };
 
-  }
-  module.exports = azure;
+
+  azure.prototype.read = function(readQuery, callback) {
+
+    //  logger.log('Azure - Read function');
+    if(readQuery.timestamp) {
+      self.dataQuery = azureTable.Query.create('PartitionKey', '==', readQuery.sensor_id).and('timestamp', '>=', readQuery.timestamp);
+    } else {
+      self.dataQuery = azureTable.Query.create('PartitionKey', '==', readQuery.sensor_id);
+    }
+
+    self.client.queryEntities(self.config.table, {
+      query: self.dataQuery
+      // onlyFields: ['sensor_id', 'timestamp', 'value']
+    }, function(err, results, continuation) {
+      if(err) {
+        logger.error('Azure - Data read failed');
+      } else {
+        _.map(results, function(d) {delete d.Timestamp; delete d.RowKey; delete d.PartitionKey});
+        logger.info("Azure - Data recieved: %d", results.length);
+        //logger.info(results);
+      }
+      callback(err, results);
+    });
+  };
+
+  azure.prototype.delete = function() {
+    self.client.deleteTable(self.config.table, function (err, resp) {
+      if(err) logger.error(err);
+      else logger.info('Table deleted');
+    });
+  };
+
+}
+module.exports = azure;
