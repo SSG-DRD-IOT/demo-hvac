9350dd590e5b154457a23c9264248c5afcd7ee1d
Daniel Holmlund <agnathan@gmail.com>
2015-09-08 11:10:48 -0700

    Added CodeMirror to the Triggers Add Page

--- devices/modules/intel-comm-components-relay/index.js
+++ devices/modules/intel-comm-components-relay/index.js
@@ -4,18 +4,18 @@ var mraa = require('mraa');
 
 // Function which can be exported from main javascrpit file
 module.exports = {
-    
+
     //Initialization of Pin
     initializePin: function(pinNo){
         var myDigitalPin = new mraa.Gpio(parseInt(pinNo)); //setup digital read on pin
         myDigitalPin.dir(mraa.DIR_OUT); //set the gpio direction to output
     },
-    
+
     // Exporting JSON file containing API, description etc.
     getApi: function(){
         return(relay);
     },
-    
+
     // Function to perform action based on trigger
     action: function(pinNo,status){
         var myDigitalPin = new mraa.Gpio(parseInt(pinNo)); //setup digital read on pin
@@ -28,18 +28,18 @@ module.exports = {
                 myDigitalPin.write(0); //set the digital pin to high (1)
             }
         }
-    }, 
+    },
 
     // Function to read current status of Pin
     readPin: function(pinNo){
         var myDigitalPin = new mraa.Gpio(parseInt(pinNo)); //setup digital read on pin
         var myDigitalValue =  myDigitalPin.read(); //read the digital value of the pin
-        return(myDigitalValue) 
+        return(myDigitalValue)
     },
-	
-	
-	returnType : function() {
-    var configs = require('./relay.json');
-    return configs.type;
-	}
+
+
+    returnType : function() {
+        var configs = require('./relay.json');
+        return configs.type;
+    }
 };
